cb9d01ab88f39651ff92fb5b2c1cc98235e40bd8
darya-semianiak <ulis9242@gmail.com>
2025-04-27 10:30:54 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -8,4 +8,15 @@ burgerMenu.addEventListener('click', () => {
     menuBody.classList.toggle('active');
     burgerMenu.classList.toggle('burger-icon');
     burgerMenu.classList.toggle('close-icon');
+
+    const isBurgerMenuOpened = menuBody.classList.contains('active');
+    if (isBurgerMenuOpened) {
+        document.body.style.top = `-${window.scrollY}px`;
+        document.body.style.position = 'fixed';
+    } else {
+        const scrollY = document.body.style.top;
+        document.body.style.position = '';
+        document.body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+    }
 });
